a821f7f9655fed9e33ebb0c54b047c36be571cd6
cesarlubini <cesar.lubini@gmail.com>
2020-12-02 07:11:06 -0300

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import PokemonCard from './components/PokemonCard/PokemonCard';
 
@@ -13,40 +12,6 @@ function App() {
       <PokemonCard/>
       <PokemonCard/>
     </>
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  // fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
-  //   .then(response => response.json())
-  //   .then(data => data.results)
-  //   .then(pokemons => console.log(pokemons))
-
-
-
-  fetch('https://pokeapi.co/api/v2/pokemon/1')
-  .then(response => response.json())
-  .then(pokemons => console.log(pokemons))
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> 5e30fc24f6c442678dc963dd43af2f9aaafc1e42
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-=======
-import reportWebVitals from './reportWebVitals';
->>>>>>> 5e30fc24f6c442678dc963dd43af2f9aaafc1e42
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,11 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-<<<<<<< HEAD
-=======
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> 5e30fc24f6c442678dc963dd43af2f9aaafc1e42
